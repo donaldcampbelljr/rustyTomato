36a05012c9172b5ecbdecea2ba58f8d25b5e327f
“donaldcampbelljr” <125581724+donaldcampbelljr@users.noreply.github.com>
2023-11-10 04:59:13 -0500

    Fix quit and break behavior.

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ pub enum TimeUnits{
     Seconds(),
 }
 
-enum TimeUnitOrBreak<'a> {
-    Str(&'a str),
+enum TimeUnitOrBreak {
+    Str(String),
     TimeItem(u64, TimeUnits),
 }
 
@@ -88,7 +88,7 @@ fn create_time(time_numbers: u64, time_tuple: TimeUnits) -> u64 {
 
 }
 
-fn get_time_input() -> TimeUnitOrBreak<'static> {
+fn get_time_input() -> TimeUnitOrBreak {
 
     // Prompting the User
 
@@ -104,29 +104,39 @@ fn get_time_input() -> TimeUnitOrBreak<'static> {
 
     println!("");
 
+
+    // Note, had to use match statement and pass a String to TimeUnitorBreak not a &str
+    // because the &str will no longer exist out of scope but the String will?
+
     let lc_input_str = input_str.to_lowercase(); // makes lower case
-    if input_str == "quit" {
-        TimeUnitOrBreak::Str(lc_input_str.as_str());
+
+    match lc_input_str.as_str(){
+        "quit\n" => TimeUnitOrBreak::Str(lc_input_str),
+        _ => {
+            let mut split_input_iter = lc_input_str.trim().split_whitespace(); // creates an iterable
+
+            let time_number= split_input_iter.next().unwrap_or_default();// takes the 'next' str in the iterable (this is the first one)
+            let time_number: u64 = time_number.parse().unwrap();
+            // *time_number as u64;
+            let time_unit = split_input_iter.next().unwrap_or_default().to_string(); // takes the 'next' str in the iterable (this is the second one)
+
+            let time_units = match time_unit.as_str(){
+                "min" => TimeUnits::Minutes(),
+                "minutes" => TimeUnits::Minutes(),
+                "m" => TimeUnits::Minutes(),
+                "seconds" => TimeUnits::Seconds(),
+                "sec" => TimeUnits::Seconds(),
+                "s" => TimeUnits::Seconds(),
+                _ => TimeUnits::Minutes(), // Basically just assume minutes
+            };
+
+            TimeUnitOrBreak::TimeItem(time_number, time_units)
+
+        }
+
     }
 
-    let mut split_input_iter = lc_input_str.trim().split_whitespace(); // creates an iterable
 
-    let time_number= split_input_iter.next().unwrap_or_default();// takes the 'next' str in the iterable (this is the first one)
-    let time_number: u64 = time_number.parse().unwrap();
-    // *time_number as u64;
-    let time_unit = split_input_iter.next().unwrap_or_default().to_string(); // takes the 'next' str in the iterable (this is the second one)
-
-     let time_units = match time_unit.as_str(){
-        "min" => TimeUnits::Minutes(),
-        "minutes" => TimeUnits::Minutes(),
-        "m" => TimeUnits::Minutes(),
-        "seconds" => TimeUnits::Seconds(),
-        "sec" => TimeUnits::Seconds(),
-        "s" => TimeUnits::Seconds(),
-        _ => TimeUnits::Minutes(), // Basically just assume minutes
-    };
-
-    TimeUnitOrBreak::TimeItem(time_number, time_units)
 
 }
 
